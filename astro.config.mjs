dccae02f02d102868c7ebf60dc19dc58bd34174b
kiri931 <44487096+kiri931@users.noreply.github.com>
2026-02-21 16:43:35 +0900

    Update Astro static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react()],
 
   vite: {
